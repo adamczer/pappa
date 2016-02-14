83ab8327126b02d54c6c29da84fc751eafde84b9
yinyan <yinyan@buffalo.edu>
2016-02-14 17:42:04 -0500

    add a deploy sub dir for collecting all binaries, and executing

---
+++ paparazzi-deploy/run.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+
+echo "> Copyign jar and so files ..."
+cp ../paparazzi-jni/target/paparazzi-jni-1.0-SNAPSHOT.jar .
+cp ../paparazzi-jni/bin/*.so .
+cp ../paparazzi-simulator/target/paparazzi-nps-1.0-SNAPSHOT.jar .
+
+echo "> java -cp paparazzi-jni-1.0-SNAPSHOT.jar:paparazzi-nps-1.0-SNAPSHOT.jar"
+echo ">      juav.simulator.nps.cyclic.NpsCyclicImpl"
+
+echo ">>Execution Outputs <<"
+
+java -cp paparazzi-jni-1.0-SNAPSHOT.jar:paparazzi-nps-1.0-SNAPSHOT.jar \
+    juav.simulator.nps.cyclic.NpsCyclicImpl
