f76db7298ec9ac01d0251b89b3775d8a6138bdc0
adamczer <adamczer@buffalo.edu>
2016-03-04 08:52:51 -0500

    addition of cfiles that seem to break makefile

---
+++ paparazzi-jni/jni/ub_cse_juav_jni_fdm_JniFdm.c
@@ -0,0 +1,16 @@
+#include <jni.h>
+#include <stdio.h>
+#include "ub_cse_juav_jni_fdm_JniFdm.h"
+
+JNIEXPORT void JNICALL Java_ub_cse_juav_jni_fdm_JniFdm_init
+  (JNIEnv *env, jclass thisClass) {
+	return;
+  }
+
+//JNIEXPORT jdouble JNICALL Java_ub_cse_juav_jni_fdm_JniFdm_fetchTime
+//  (JNIEnv *env, jclass thisClass) {
+//  	return 1.0;
+//  }
+
+
+
