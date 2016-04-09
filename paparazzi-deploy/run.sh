cfa8323d96630ddcdc8d64bc8379008d8cd1c68d
Yin <yinyan@buffalo.edu>
2016-04-09 13:21:31 -0400

    path fix

--- paparazzi-deploy/run.sh
+++ paparazzi-deploy/run.sh
@@ -1,19 +1,18 @@
 #! /bin/bash
 
-export LD_LIBRARY_PATH=/home/gassa/workspace/juav-pappa/paparazzi-jni/libs
+export LD_LIBRARY_PATH=.
 
 echo "> Copyign jar and so files ..."
 cp ../paparazzi-jni/target/paparazzi-jni-1.0-SNAPSHOT.jar .
 cp ../paparazzi-jni/bin/*.so .
 cp ../paparazzi-simulator/target/paparazzi-nps-1.0-SNAPSHOT.jar .
 
-export LD_LIBRARY_PATH=/home/gassa/workspace/juav-pappa/paparazzi-jni/libs
 
 echo "> java -cp paparazzi-jni-1.0-SNAPSHOT.jar:paparazzi-nps-1.0-SNAPSHOT.jar"
 echo ">      juav.simulator.nps.cyclic.NpsCyclicImpl"
 
 echo ">>Execution Outputs <<"
 
-java -Djava.library.path=/home/gassa/workspace/juav-pappa/paparazzi-jni/libs \
+java -Djava.library.path=/home/gassa/uav/jUAV/paparazzi-deploy/libs \
     -cp paparazzi-jni-1.0-SNAPSHOT.jar:paparazzi-nps-1.0-SNAPSHOT.jar \
     juav.simulator.nps.cyclic.NpsCyclicImpl
