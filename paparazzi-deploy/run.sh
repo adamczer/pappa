7174357026f7d1ceb2d685afb9fc3414e4d32c01
Yin <yinyan@buffalo.edu>
2016-04-27 16:32:49 -0400

    change executing scripts

--- paparazzi-deploy/run.sh
+++ paparazzi-deploy/run.sh
@@ -8,11 +8,8 @@ cp ../paparazzi-jni/bin/*.so .
 cp ../paparazzi-simulator/target/paparazzi-nps-1.0-SNAPSHOT.jar .
 
 
-echo "> java -cp paparazzi-jni-1.0-SNAPSHOT.jar:paparazzi-nps-1.0-SNAPSHOT.jar"
-echo ">      juav.simulator.nps.cyclic.NpsCyclicImpl"
-
 echo ">>Execution Outputs <<"
 
-java -Djava.library.path=/home/gassa/uav/jUAV/paparazzi-deploy/libs \
+java -Djava.library.path=../paparazzi-jni/libs \
     -cp paparazzi-jni-1.0-SNAPSHOT.jar:paparazzi-nps-1.0-SNAPSHOT.jar \
     juav.simulator.nps.cyclic.NpsCyclicImpl
